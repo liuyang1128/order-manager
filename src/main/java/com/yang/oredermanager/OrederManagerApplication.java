8cef482ae9948c794a8d8766d8d0e19fa74345ac
liuyang <491154804@qq.com>
2018-09-09 11:51:31 +0800

    项目初次提交

---
+++ src/main/java/com/yang/oredermanager/OrederManagerApplication.java
@@ -0,0 +1,12 @@
+package com.yang.oredermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrederManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrederManagerApplication.class, args);
+    }
+}
